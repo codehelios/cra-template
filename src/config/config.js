5e6e936e8912a0c2eac5c0fc5e08e7c17fa70eb4
Marishwaran99 <marishwaran22@gmail.com>
2020-10-19 11:37:40 +0530

    created template folder

--- src/config/config.js
+++
@@ -1,3 +0,0 @@
-export const firebaseConfig = {
-	message: 'REMOVE THIS OBJECT AND REPLACE WITH ACTUAL FIREBASE CONFIG.',
-};
---
+++ template/src/config/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyB7-VYbktv4wy5hfjKi0apD9wrdiy4enyc",
+	authDomain: "testapp-27db9.firebaseapp.com",
+	databaseURL: "https://testapp-27db9.firebaseio.com",
+	projectId: "testapp-27db9",
+	storageBucket: "testapp-27db9.appspot.com",
+	messagingSenderId: "340028784727",
+	appId: "1:340028784727:web:797d4f5c35ef944796a312",
+	measurementId: "G-CGFC802M47"
+  };
